d521d1f7d4b2170a5e70a386980a1c19b7cab533
SyInvidia <komacine@protonmail.com>
2022-01-02 19:44:04 -0600

    fix build errs

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -4,11 +4,10 @@ import cloudflare from '@sveltejs/adapter-cloudflare';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: cloudflare(),
+		adapter: cloudflare({ platform: 'node' }),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-
 		vite: {
 			build: {
 				target: 'esnext'
