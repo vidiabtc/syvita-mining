7b85d86da1c3889b6f67124fdae5c489e57a7d92
diopitis <wgto0ybe7@relay.firefox.com>
2022-01-18 11:35:04 -0600

    added email subscribe

--- ui/src/components/pool/emailSubscribe.svelte
+++ ui/src/components/pool/emailSubscribe.svelte
@@ -1,28 +1,32 @@
 <script>
 	let email;
 
-	const submitForm = async () => {
-		const submit = await fetch('api/email', {
-			body: JSON.stringify({
-				email
-			})
-		});
+	const submitEmail = async (email) => {
+		let url = `https://campaigns.zoho.com/api/v1.1/addlistsubscribersinbulk?listkey=3z12320c6e15396486ff571cefc8a17dcaf67687a536e02bbd63cbb654293a2260&resfmt=JSON&emailids=${email}`;
 
-		const data = await submit.json();
-		console.log(data);
+		let res = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				Authorization:
+					'Zoho-oauthtoken 1000.6966def5d0dd2b4e74ba49b2aadcd4d4.d1af1919199ebd76581433850c979585'
+			}
+		});
+		let data = await res.json();
+		console.log('data', data);
 	};
 </script>
 
-<form on:submit={submitForm}>
-	<label>
-		Email
-		<input type="text" name="email" bind:value={email} />
-	</label>
-	<input type="submit" />
-</form>
+<div class="email-wrapper">
+	<h3>Email</h3>
+	<form on:submit|preventDefault={() => submitEmail(email)}>
+		<input bind:value={email} type="email" />
+		<input type="submit" />
+	</form>
+</div>
 
 <style>
-	form {
+	.email-wrapper {
 		margin-top: 300px;
 		margin-right: auto;
 		margin-left: auto;
---
+++ ui/src/routes/test.svelte
@@ -0,0 +1,5 @@
+<script>
+	import EmailSubscribe from '$components/pool/emailSubscribe.svelte';
+</script>
+
+<EmailSubscribe />
