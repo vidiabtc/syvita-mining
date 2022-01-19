0da05e105729a3b1701c685203c33f9fc6dd0267
diopitis <wgto0ybe7@relay.firefox.com>
2022-01-19 10:34:55 -0600

    started work on admin page

---
+++ ui/src/components/admin/claimMany.svelte
@@ -0,0 +1,139 @@
+<script>
+	let numOfBlocks = 0;
+	let stxPerBlock = 0;
+</script>
+
+<div class="main-wrapper">
+	<h2>contract-claim-many</h2>
+	<div class="contract-call-wrapper">
+		<div class="input-label">
+			<div class="input-field">
+				<p>Mine many Id</p>
+				<input bind:value={stxPerBlock} type="number" />
+			</div>
+		</div>
+		<div class="input-label">
+			<div class="input-field">
+				<p>Block number(seperate blocks with commas)</p>
+				<input bind:value={stxPerBlock} type="number" />
+			</div>
+		</div>
+
+		<div class="submit">
+			<button
+				on:click={() => {
+					numOfBlocks = 0;
+					stxPerBlock = 0;
+				}}
+				class="reset-button">Reset</button
+			>
+			<button on:click={() => mineMany(numOfBlocks, stxPerBlock * 1000000)} class="submit-button"
+				>Claim Many</button
+			>
+		</div>
+	</div>
+</div>
+
+<style>
+	.main-wrapper {
+		max-width: 600px;
+
+		padding-top: 40px;
+	}
+
+	h2 {
+		font-size: 2rem;
+		padding-bottom: 15px;
+	}
+
+	.contract-call-wrapper {
+		height: max-content;
+		border: 2px solid transparent;
+		border-radius: 10px;
+		border: 1px solid blue;
+		padding: 20px;
+	}
+
+	.input-label {
+		padding: 20px;
+		position: relative;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.input-field {
+		max-width: 569px;
+		width: 100%;
+		min-width: 150px;
+	}
+
+	input {
+		color: white;
+		width: 100%;
+		height: 50px;
+		padding-left: 10px;
+	}
+
+	p {
+		font-size: 0.6rem;
+		top: 15px;
+		left: 30px;
+		position: absolute;
+		background: #050810;
+		padding-left: 5px;
+		padding-right: 5px;
+	}
+
+	.submit {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		gap: 20px;
+	}
+
+	.reset-button {
+		width: 170px;
+		height: 50px;
+		border: 1px solid #ffffff;
+		margin-left: 20px;
+		margin-top: 20px;
+	}
+
+	.submit-button {
+		max-width: 170px;
+		min-width: 80px;
+		height: 50px;
+		background: #384cff;
+		margin-right: 20px;
+		margin-top: 20px;
+	}
+
+	.submit-button:hover {
+		background-color: rgba(56, 76, 255, 0.9);
+		cursor: pointer;
+	}
+
+	.submit-button:active {
+		background-color: rgba(56, 76, 255, 0.8);
+	}
+
+	input::-webkit-outer-spin-button,
+	input::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+
+	/* Firefox */
+	input[type='number'] {
+		-moz-appearance: textfield;
+	}
+
+	@media (max-width: 400px) {
+		.contract-call-wrapper {
+			padding: 10px;
+		}
+		.submit {
+			max-width: 295px;
+		}
+	}
+</style>
---
+++ ui/src/routes/admin.svelte
@@ -0,0 +1,72 @@
+<script>
+	import SelectCity from '$components/selectCity.svelte';
+	import MineMany from '$components/admin/mineMany.svelte';
+	import EnableClaiming from '$components/admin/enableClaiming.svelte';
+	import SetPool from '$components/admin/setPool.svelte';
+	import SetEndBlock from '$components/admin/setEndBlock.svelte';
+	import AddAdmin from '$components/admin/addAdmin.svelte';
+	import RemoveAdmin from '$components/admin/removeAdmin.svelte';
+	import UpdateFeeAddress from '$components/admin/updateFeeAddress.svelte';
+	import ClaimSingle from '$components/admin/claimSingle.svelte';
+	import ClaimMany from '$components/admin/claimMany.svelte';
+</script>
+
+<div class="contract-calls-wrapper">
+	<div class="title">
+		<h1>Welcome Admin</h1>
+		<p>
+			Below you will find contract calls for the different mining contracts. Please select the
+			contract below you would like to interact with.
+		</p>
+	</div>
+	<div class="select-city-wrapper">
+		<SelectCity />
+	</div>
+	<div class="contract-calls">
+		<MineMany />
+		<EnableClaiming />
+		<SetPool />
+		<SetEndBlock />
+		<AddAdmin />
+		<RemoveAdmin />
+		<UpdateFeeAddress />
+		<ClaimSingle />
+		<ClaimMany />
+	</div>
+</div>
+
+<style>
+	.contract-calls-wrapper {
+		padding: 0 20px;
+	}
+
+	.title {
+		margin-left: 50px;
+		padding-top: 100px;
+		max-width: 800px;
+	}
+
+	h1 {
+		font-size: 3rem;
+		padding-bottom: 10px;
+	}
+
+	.title p {
+		font-size: 1.2rem;
+	}
+	.select-city-wrapper {
+		padding-top: 25px;
+		padding-bottom: 50px;
+		width: 212px;
+	}
+
+	.contract-calls {
+		max-width: 600px;
+		margin: auto;
+	}
+	@media (max-width: 600px) {
+		.title {
+			margin-left: 0px;
+		}
+	}
+</style>
