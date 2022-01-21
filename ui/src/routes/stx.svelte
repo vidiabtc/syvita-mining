2359c34166b73e361ffa90f603c54b580b0adb11
diopitis <wgto0ybe7@relay.firefox.com>
2022-01-21 13:35:03 -0600

    btcpay_server_stuff

---
+++ ui/src/routes/stx.svelte
@@ -0,0 +1,32 @@
+<!-- <script>
+  // import btcpay from 'btcpay';
+  // const btcpay = import('btcpay')
+  // const btcpay = require('btcpay')
+
+
+  // const keypair = btcpay.crypto.load_keypair(new Buffer.from(import.meta.env.VITE_BTCPAYSERVER_KEY, 'hex'))
+
+  // https://mainnet.demo.btcpayserver.org
+
+  //${import.meta.env.VITE_BTCPAYSERVER_KEY}
+
+  //VITE_BTCPAYSERVER_MERCHANT
+
+//   const getStore = async () => {
+//     let url = 'https://demo.btcpayserver.org/api/v1/stores/GMRWXB7xAZBrWkgnBxoqVrQrCC2HGzgT9zyBD5jeT8XX'
+//     let res = await fetch(url, {
+// 		method: 'GET',
+// 		headers: { 'Content-Type': 'application/json', 'Authorization': `token ${import.meta.env.VITE_BTCPAYSERVER_KEY}`},
+// 	  });
+//     let data = await res.json()
+//     console.log('Data ', data)
+//   }
+
+// </script> -->
+
+
+// <h1>STX Mining</h1>
+// <button on:click={getStore}>Get Store</button>
+
+
+// <!-- <h1>{JSON.stringify(keypair)}</h1> -->
