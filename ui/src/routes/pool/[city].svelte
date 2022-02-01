1a8106ffdd65f370201f41fb3cbe803931aa0ff1
diopitis <wgto0ybe7@relay.firefox.com>
2022-01-31 18:13:27 -0600

    decentralize ugh

--- ui/src/routes/pool/[city].svelte
+++ ui/src/routes/pool/[city].svelte
@@ -3,9 +3,9 @@
 
 	export async function load({ page, fetch }) {
 		let city = page.params.city;
-    if (city != 'stx') {
-      city = CITIES[city]
-    }
+		if (city != 'stx') {
+			city = CITIES[city];
+		}
 		if (!city) {
 			return {
 				status: 303,
@@ -14,7 +14,7 @@
 		} else {
 			return {
 				props: {
-					city,
+					city
 				}
 			};
 		}
@@ -25,59 +25,21 @@
 	import PoolStats from '$components/pool/poolStats.svelte';
 	import PoolHistory from '$components/pool/poolHistory.svelte';
 	import PoolSelectCity from '$components/pool/PoolSelectCity.svelte';
-  import SelectCity from '$components/selectCity.svelte';
+	import SelectCity from '$components/selectCity.svelte';
 	import EmailSubscribe from '$components/pool/emailSubscribe.svelte';
 	import { user, t } from '$lib/stores.js';
 	import { getLatestPoolId, getPool, getBlockHeight } from '$lib/apiCalls';
-  import StxPoolStats from '$components/stx/poolStats.svelte';
-  import Contribute from '$components/stx/contribute.svelte';
-  import { getStore, createInvoice, getInvoices } from '$lib/btcpay.js'
+	import StxPoolStats from '$components/stx/poolStats.svelte';
+	import Contribute from '$components/stx/contribute.svelte';
+	import { getStore, createInvoice, getInvoices } from '$lib/btcpay.js';
 	import { getStxAddress } from '$lib/auth';
 
-  export let city;
+	export let city;
 
 	$: stxAddress = getStxAddress($user);
 	// $: stxAddress = 'SP3YXDXXHX1KQWG7N7G9WJQR69QGYN6DR1NK5H8XK';
 	$: poolId = getLatestPoolId(city);
 	$: blockHeight = getBlockHeight(city);
-
-	// TEMPORARY FOR MANUAL POOL
-
-	import { onMount } from 'svelte';
-	import Modal from '$components/modal.svelte';
-
-  let poolAddress = 'SP78Q12M26WVN1V9DPQ29HVDTWPKQH6KVR1X0VEW';
-	let isModalOpen = false;
-	let contributeAmount;
-	let totalContributed = -1;
-	const toggleModal = () => (isModalOpen = !isModalOpen);
-  onMount(async () => {
-    let url = `https://mainnet.syvita.org/extended/v1/address/${poolAddress}/balances?unanchored=true`;
-    let res = await fetch(url);
-    let balance = await res.json();
-    url = `https://mainnet.syvita.org/extended/v1/address/${poolAddress}/balances`;
-    res = await fetch(url);
-    let test = await res.json();
-    url = `https://mainnet.syvita.org/extended/v1/address/${poolAddress}/mempool`;
-    res = await fetch(url);
-    let data = await res.json();
-    let mempool = data.results;
-    let pendingBalance = 0;
-    for (let i = 0; i < mempool.length; i++) {
-      if (
-        mempool[i].sender_address != poolAddress &&
-        mempool[i].tx_status == 'pending' &&
-        mempool[i].tx_type == 'token_transfer'
-      ) {
-        pendingBalance += Math.floor(parseInt(mempool[i].token_transfer.amount) / 1000000);
-      }
-    }
-    console.log(balance.stx.total_received - test.stx.total_received / 1000000);
-    console.log(pendingBalance);
-    // console.log(pendingBalance)
-    totalContributed = Math.floor(balance.stx.total_received / 1000000) + pendingBalance;
-  });
-	/////////////////
 </script>
 
 <svelte:head>
@@ -89,47 +51,20 @@
 </svelte:head>
 
 {#if city == 'stx'}
-  <StxPoolStats/>
-
+	<StxPoolStats />
 {:else}
-   
-<div class="pool-wrapper">
-	<div class="select-city">
-		<PoolSelectCity selectedCity={city.coin}/>
-	</div>
-	{#if city.coin == 'mia'}
+	<div class="pool-wrapper">
+		<div class="select-city">
+			<PoolSelectCity selectedCity={city.coin} />
+		</div>
 		{#await poolId}
 			<h1>loading...</h1>
 		{:then poolId}
-			<PoolStats city={city} {poolId} {blockHeight} {stxAddress} />
-			<PoolHistory city={city} {poolId} />
+			<PoolStats {city} {poolId} {blockHeight} {stxAddress} />
+			<PoolHistory {city} {poolId} />
 			<EmailSubscribe />
 		{/await}
-	{:else}
-		<div class="toggle-wrapper">
-			<h1>NYC Pool 5</h1>
-			{#if totalContributed > -1}
-				<h2>Total Contributed: {totalContributed.toLocaleString()} STX</h2>
-			{:else}
-				<h2>Total Contributed: loading...</h2>
-			{/if}
-			<h4>Cap: 1,000,000 STX</h4>
-
-			<div class="contribute-input">
-				<input placeholder="Pool Closed" bind:value={contributeAmount} type="number" />
-				{#if contributeAmount >= 40 && totalContributed < 1000000}
-					<button disabled=true >Add Funds</button>
-				{:else}
-					<button>Add Funds</button>
-				{/if}
-			</div>
-		</div>
-		<EmailSubscribe />
-		{#if isModalOpen}
-			<Modal on:close={toggleModal} coin="nyc" {contributeAmount} />
-		{/if}
-	{/if}
-</div>
+	</div>
 {/if}
 
 <style>
@@ -145,62 +80,6 @@
 		margin-left: auto;
 		margin-right: 20px;
 		padding-bottom: 35px;
-	}
-
-	/* TEMPORARY STYLES FOR MANUAL POOL */
-	.toggle-wrapper {
-		padding-top: 50px;
-		color: white;
-		text-align: center;
-		font-size: 1.5rem;
-	}
-	.contribute-input {
-		padding-top: 50px;
-		display: flex;
-		justify-content: center;
-	}
-	.contribute-input button {
-		margin-left: 5px;
-	}
-	input {
-		height: 40px;
-		width: 200px;
-		border: 1px solid lightgray;
-		border-radius: 10px;
-		transition: 0.4s;
-		font-size: 1.25rem;
-	}
-	input:focus {
-		color: black;
-		outline: none;
-		background-color: lightgrey;
-	}
-	button {
-		height: 40px;
-		width: 200px;
-		border-radius: 10px;
-		transition: 0.4s;
-		border: none;
-		cursor: pointer;
-		font-size: 1.5rem;
-		background: #384cff;
-	}
-	button:hover {
-		background-color: rgba(56, 76, 255, 0.9);
-		cursor: pointer;
-	}
-
-	button:active {
-		background-color: rgba(56, 76, 255, 0.8);
-	}
-	input::-webkit-outer-spin-button,
-	input::-webkit-inner-spin-button {
-		-webkit-appearance: none;
-		margin: 0;
-	}
-
-	::placeholder {
-		padding-left: 10px;
 	}
 
 	@media (max-width: 768px) {
