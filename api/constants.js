4f79e73c360bdb6209e96bdfca1f4143c055e606
kj <komacine@protonmail.com>
2022-05-18 13:53:22 +0100

    fixed api for v2

--- api/constants.js
+++ api/constants.js
@@ -10,8 +10,8 @@ export const CITIES = {
     coin: 'mia',
     img: 'citycoins/mia.svg',
     cityWalletAddress: 'SM2MARAVW6BEJCD13YV2RHGYHQWT7TDDNMNRB1MVT',
-    contractAddress: 'SP466FNC0P7JWTNM2R9T199QRZN1MYEDTAR0KP27',
-    contractName: 'miamicoin-core-v1',
+    contractAddress: 'SP1H1733V5MZ3SZ9XRW9FKYGEZT0JDGEB8Y634C7R',
+    contractName: 'miamicoin-core-v2',
     token:
       'SP466FNC0P7JWTNM2R9T199QRZN1MYEDTAR0KP27.miamicoin-token::miamicoin',
     miningHistoryUrl: 'miamining.com',
@@ -22,8 +22,8 @@ export const CITIES = {
     coin: 'nyc',
     img: 'citycoins/nyc.svg',
     cityWalletAddress: 'SM18VBF2QYAAHN57Q28E2HSM15F6078JZYZ2FQBCX',
-    contractAddress: 'SP2H8PY27SEZ03MWRKS5XABZYQN17ETGQS3527SA5',
-    contractName: 'newyorkcitycoin-core-v1',
+    contractAddress: 'SM18VBF2QYAAHN57Q28E2HSM15F6078JZYZ2FQBCX',
+    contractName: 'newyorkcitycoin-core-v2',
     token:
       'SP2H8PY27SEZ03MWRKS5XABZYQN17ETGQS3527SA5.newyorkcitycoin-token::newyorkcitycoin',
     miningHistoryUrl: 'mining.nyc',
